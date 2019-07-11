207642f88cd47878d0163095975d60221787cddf
aajinu <apriliantoajin@gmail.com>
2019-07-12 03:11:53 +0700

    landing page

--- application/views/landingpage/index.php
+++ application/views/landingpage/index.php
@@ -438,7 +438,7 @@
 				<!-- section title -->
 				<div class="col-md-12">
 					<div class="section-title">
-						<h2 class="title">Deals Of The Day</h2>
+						<h2 class="title">Undangan Khitanan</h2>
 						<div class="pull-right">
 							<div class="product-slick-dots-2 custom-dots">
 							</div>
@@ -447,40 +447,17 @@
 				</div>
 				<!-- section title -->
 
-				<!-- Product Single -->
+				<!-- banner -->
 				<div class="col-md-3 col-sm-6 col-xs-6">
-					<div class="product product-single product-hot">
-						<div class="product-thumb">
-							<div class="product-label">
-								<span class="sale">-20%</span>
-							</div>
-							<ul class="product-countdown">
-								<li><span>00 H</span></li>
-								<li><span>00 M</span></li>
-								<li><span>00 S</span></li>
-							</ul>
-							<button class="main-btn quick-view"><i class="fa fa-search-plus"></i> Quick view</button>
-							<img src="./img/product01.jpg" alt="">
-						</div>
-						<div class="product-body">
-							<h3 class="product-price">$32.50 <del class="product-old-price">$45.00</del></h3>
-							<div class="product-rating">
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star"></i>
-								<i class="fa fa-star-o empty"></i>
-							</div>
-							<h2 class="product-name"><a href="#">Product Name Goes Here</a></h2>
-							<div class="product-btns">
-								<button class="main-btn icon-btn"><i class="fa fa-heart"></i></button>
-								<button class="main-btn icon-btn"><i class="fa fa-exchange"></i></button>
-								<button class="primary-btn add-to-cart"><i class="fa fa-shopping-cart"></i> Add to Cart</button>
-							</div>
+					<div class="banner banner-2">
+						<img src="<?= base_url('assets/landingpage/img/') ?>/banner14.jpg" alt="">
+						<div class="banner-caption">
+							<h2 class="white-color">NEW<br>COLLECTION</h2>
+							<button class="primary-btn">Shop Now</button>
 						</div>
 					</div>
 				</div>
-				<!-- /Product Single -->
+				<!-- /banner -->
 
 				<!-- Product Slick -->
 				<div class="col-md-9 col-sm-6 col-xs-6">
@@ -490,7 +467,7 @@
 							<div class="product product-single">
 								<div class="product-thumb">
 									<button class="main-btn quick-view"><i class="fa fa-search-plus"></i> Quick view</button>
-									<img src="./img/product06.jpg" alt="">
+									<img src="<?= base_url('assets/landingpage/img/') ?>/product06.jpg" alt="">
 								</div>
 								<div class="product-body">
 									<h3 class="product-price">$32.50</h3>
@@ -518,7 +495,7 @@
 										<span class="sale">-20%</span>
 									</div>
 									<button class="main-btn quick-view"><i class="fa fa-search-plus"></i> Quick view</button>
-									<img src="./img/product05.jpg" alt="">
+									<img src="<?= base_url('assets/landingpage/img/') ?>/product05.jpg" alt="">
 								</div>
 								<div class="product-body">
 									<h3 class="product-price">$32.50 <del class="product-old-price">$45.00</del></h3>
@@ -543,7 +520,7 @@
 							<div class="product product-single">
 								<div class="product-thumb">
 									<button class="main-btn quick-view"><i class="fa fa-search-plus"></i> Quick view</button>
-									<img src="./img/product04.jpg" alt="">
+									<img src="<?= base_url('assets/landingpage/img/') ?>/product04.jpg" alt="">
 								</div>
 								<div class="product-body">
 									<h3 class="product-price">$32.50</h3>
@@ -572,7 +549,7 @@
 										<span class="sale">-20%</span>
 									</div>
 									<button class="main-btn quick-view"><i class="fa fa-search-plus"></i> Quick view</button>
-									<img src="./img/product03.jpg" alt="">
+									<img src="<?= base_url('assets/landingpage/img/') ?>/product03.jpg" alt="">
 								</div>
 								<div class="product-body">
 									<h3 class="product-price">$32.50 <del class="product-old-price">$45.00</del></h3>
